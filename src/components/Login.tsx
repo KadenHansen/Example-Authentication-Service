1af21fb2783d8a210cd8d40f551fe34b01d19743
Kaden Hansen <kadendeanhansen@gmail.com>
2022-08-29 13:40:00 -0400

    add login form submit handler function

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import * as Yup from 'yup'
+import { useNavigate } from 'react-router-dom'
+import { login } from '../services/auth.service'
 
 const Login: React.FC = () => {
+
     const initialValues: {
         username: string,
         password: string
@@ -9,10 +12,38 @@ const Login: React.FC = () => {
         username: '',
         password: ''
     }
+
     const validationSchema = Yup.object().shape({
         username: Yup.string().required('This field is required!'),
         password: Yup.string().required('This field is required!')
     })
+
+    let navigate = useNavigate()
+    const [loading, setLoading] = useState<boolean>(false)
+    const [message, setMessage] = useState<string>('')
+
+    const handleLogin = (formValue: { username: string, password: string }) => {
+    const {username, password} = formValue
+    setMessage('')
+    setLoading(true)
+    login(username, password).then(
+        () => {
+            navigate("/profile")
+            window.location.reload()
+        }
+    ),
+    (err: { response: { data: { message: any } }; message: any; toString: () => any }) => {
+        const resMessage = (
+            err.response &&
+            err.response.data &&
+            err.response.data.message ||
+            err.message ||
+            err.toString()
+        )
+        setLoading(false)
+        setMessage(resMessage)
+    }
+}
     return (
         <div>
 
