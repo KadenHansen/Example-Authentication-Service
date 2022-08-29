ecdb942a123da8fdebf05f38e5f3bfcccde6a8fc
Kaden Hansen <kadendeanhansen@gmail.com>
2022-08-28 22:40:22 -0400

    add login method to authentication service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,10 +1,23 @@
-import axios from "axios";
+import axios from "axios"
 const API_URL = "http://localhost:3000/api/auth/"
 
+// Register method: submits POST request with username, email and password
 export const register = (username: string, email: string, password: string) => {
     return axios.post(API_URL + "signup", {
         username,
         email,
         password
+    })
+}
+
+// Login method: submits POST request with username, password. Save JWT token to local storage
+export const login = (username: string, password: string) => {
+    return axios.post(API_URL + "login", {
+        username,
+        password
+    }).then((res) => {
+        if (res.data.accessToken) {
+            localStorage.setItem("user", JSON.stringify(res.data))
+        }
     })
 }
